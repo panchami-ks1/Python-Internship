aa672d0541f099ace5abb9f41f2ec609c3c2c33a
panchami-ks1 <panchami.ks1@gmail.com>
2022-01-05 22:50:04 +0100

    Initial commit with day1, day2 sample programes.

---
+++ Day-01/test.py
@@ -0,0 +1,11 @@
+print("Heloo python");
+name="panju"
+number=123;
+my_list=['pan','name',1,2];
+my_tuple=(1,2,'panju','achu');
+print("My name is",name);
+print("Roll no:",number);
+print("name is:"+name);
+print(my_list);
+print(type(my_tuple));
+print(my_tuple);
---
+++ Day-02/type_cast.py
@@ -0,0 +1,33 @@
+# num1=123
+# num2=12.3
+# print(type(num1))
+# print(type(num2))
+# sum=num1+num2
+# print(type(sum))
+# list=[1,2,3]
+# tuple=tuple(list)
+# print(tuple)
+# num3='123'
+# num4=int(num3)
+# print(type(num3))
+# print(type(num4))
+# str1="Hello world"
+# print(str1[2])
+# print(str1[2:7])
+# print(len(str1))
+# print(str1.lower())
+# print(str1.upper())
+# print(str1.replace("Hello","hi"))
+# str2="@hello@"
+# print(str2.lstrip("@"))
+# print(str2.rstrip("@"))
+# print(str2.strip("@"))
+# x=8
+# y=9
+# print(x,y)
+# print(x,y,sep="&&")
+# print(x,end=" ")
+# print(y)
+length=10
+breadth=10
+print("Area wth length {} and breadth {} is {}sq.units".format(length,breadth,length*breadth))
